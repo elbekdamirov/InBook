2c91034733405b7a09db1394cefd600b4df44454
elbekdamirov <elbekmain@gmail.com>
2025-07-01 11:13:42 +0500

    crud

--- src/genres/genres.module.ts
+++ src/genres/genres.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
-import { GenresService } from './genres.service';
-import { GenresController } from './genres.controller';
+import { Module } from "@nestjs/common";
+import { GenresService } from "./genres.service";
+import { GenresController } from "./genres.controller";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { Genre } from "./models/genre.model";
 
 @Module({
+  imports: [SequelizeModule.forFeature([Genre])],
   controllers: [GenresController],
   providers: [GenresService],
 })
--- src/languages/languages.module.ts
+++ src/languages/languages.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
-import { LanguagesService } from './languages.service';
-import { LanguagesController } from './languages.controller';
+import { Module } from "@nestjs/common";
+import { LanguagesService } from "./languages.service";
+import { LanguagesController } from "./languages.controller";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { Language } from "./models/language.model";
 
 @Module({
+  imports: [SequelizeModule.forFeature([Language])],
   controllers: [LanguagesController],
   providers: [LanguagesService],
 })
